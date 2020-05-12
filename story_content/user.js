17962058f385da8d209efac6cdba8969c3789c17
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2020-05-12 17:09:31 +0300

    covidtask

---
+++ story_content/user.js
@@ -0,0 +1,31 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "66xKWDIz5BZ":
+        Script1();
+        break;
+      case "61cL4qOb1Xk":
+        Script2();
+        break;
+      case "65ioBQYxrVS":
+        Script3();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script3()
+{
+  window.print();
+}
+
